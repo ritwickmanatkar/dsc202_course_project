94829b9e39c01a12272e171d9ae70a7da96ca8b0
Ritwick Manatkar <ritwickmanatkar@Ritwicks-MacBook-Pro.local>
2023-12-09 11:51:57 -0800

    Added connectors for 3 databases

---
+++ auth/README.md
@@ -0,0 +1,6 @@
+# CREATE YOUR AUTH PASSWORDS IN THIS DIRECTORY
+
+## - Duplicate the sample file(connector_auth_sample.py).
+## - Rename it to 'connector_auth.py'.
+## - Fill out the usernames and passwords as directed.
+## - Make sure the file 'connector_auth.py' exists in the .gitignore file.
---
+++ connectors/postgres_connector.py
@@ -0,0 +1,33 @@
+import psycopg2
+
+from auth.connector_auth import (
+    POSTGRES_DBNAME,
+    POSTGRES_USER,
+    POSTGRES_PASSWORD,
+    POSTGRES_HOST,
+    POSTGRES_PORT
+)
+
+POSTGRESQL_CONNECTION = None
+
+
+def get_postgresql_connection_object():
+    """
+
+    :return:
+    """
+    POSTGRESQL_CONNECTION = psycopg2.connect(
+        dbname=POSTGRES_DBNAME,
+        user=POSTGRES_USER,
+        password=POSTGRES_PASSWORD,
+        host=POSTGRES_HOST,
+        port=POSTGRES_PORT
+    )
+    return POSTGRESQL_CONNECTION
+
+
+def get_postgresql_cursor_object():
+    if POSTGRESQL_CONNECTION is None:
+        return get_postgresql_connection_object().cursor()
+
+    return POSTGRESQL_CONNECTION.cursor()
